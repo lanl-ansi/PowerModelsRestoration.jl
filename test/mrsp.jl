20419cf9110df3e9b39794de7b24f810c9c0a02a
nrhodes <nrhodes@ad.engr.wisc.edu>
2019-09-20 19:40:43 -0500

    PRG: fix bug in MRSP preprocessing of ROP with SOC

--- test/mrsp.jl
+++ test/mrsp.jl
@@ -89,12 +89,48 @@
 
 
     @testset "test soc mrsp" begin
+    data = PowerModels.parse_file("../test/data/case5_restoration_strg.m")
+    PowerModelsRestoration.propagate_damage_status!(data)
+    result = Dict()
+
         @testset "5-bus strg case" begin
-            data = PowerModels.parse_file("../test/data/case5_restoration_strg.m")
-            PowerModelsRestoration.propagate_damage_status!(data)
             result = PowerModelsRestoration.run_mrsp(data, PowerModels.SOCWRPowerModel, juniper_solver)
 
             @test result["termination_status"] == LOCALLY_SOLVED
+            @test isapprox(result["objective"], 5.0; atol = 1e-2)
+
+            @test isapprox(bus_status(result,"1"), 1; atol=1e-2)
+            @test isapprox(bus_status(result,"2"), 1; atol=1e-2)
+            @test isapprox(bus_status(result,"3"), 1; atol=1e-2)
+            @test isapprox(bus_status(result,"4"), 1; atol=1e-2)
+            @test isapprox(bus_status(result,"10"), 1; atol=1e-2)
+
+            @test isapprox(gen_status(result,"1"), 0; atol=1e-2)
+            @test isapprox(gen_status(result,"2"), 0; atol=1e-2)
+            @test isapprox(gen_status(result,"3"), 1; atol=1e-2)
+            @test isapprox(gen_status(result,"4"), 1; atol=1e-2)
+            @test isapprox(gen_status(result,"5"), 1; atol=1e-2)
+
+            @test isapprox(storage_status(result,"1"), 0; atol=1e-2)
+            @test isapprox(storage_status(result,"2"), 1; atol=1e-2)
+
+            @test isapprox(branch_status(result,"1"), 1; atol=1e-2)
+            @test isapprox(branch_status(result,"2"), 0; atol=1e-2)
+            @test isapprox(branch_status(result,"3"), 1; atol=1e-2)
+            @test isapprox(branch_status(result,"4"), 0; atol=1e-2)
+            @test isapprox(branch_status(result,"5"), 0; atol=1e-2)
+            @test isapprox(branch_status(result,"6"), 0; atol=1e-2)
+            @test isapprox(branch_status(result,"7"), 1; atol=1e-2)
+        end
+
+        @testset "test ROP with MRSP preprocessing" begin
+            PowerModelsRestoration.clean_solution!(result)
+            PowerModels.update_data!(data, result["data"])
+            PowerModels.update_data!(data, result["solution"])
+
+            mn_data = build_mn_data(data, replicates=2)
+            result = PowerModelsRestoration.run_rop(mn_data, PowerModels.SOCWRPowerModel, juniper_solver)
+            @test result[" termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 5.0; atol = 1e-2)
 
             @test isapprox(bus_status(result,"1"), 1; atol=1e-2)
