571c3d2496bcb7c241fc2080e0e80029b4573e32
Noah Rhodes <noahx23@gmail.com>
2020-01-20 10:33:57 -0600

    FIX: #22 constraint_increasing_load
    The data check for the variable car(pm,n,z_storage, i) was incorrect
    and did not add the constraint to the function.  This data check
    should not be neccessary because this function should only be called
    if the model contains the relevant variable.

--- src/core/constraint.jl
+++ src/core/constraint.jl
@@ -107,12 +107,11 @@ end
 
 "Load delivered at each node must be greater than or equal the previous time period"
 function constraint_increasing_load(pm::_PMs.AbstractPowerModel,  i::Int, nw_1::Int, nw_2::Int)
-    if i in  _PMs.var(pm, nw_1, :z_demand)
-        z_demand_1 = _PMs.var(pm, nw_1, :z_demand, i)
-        z_demand_2 = _PMs.var(pm, nw_2, :z_demand, i)
 
-        JuMP.@constraint(pm.model, z_demand_2 >= z_demand_1)
-    end
+    z_demand_1 = _PMs.var(pm, nw_1, :z_demand, i)
+    z_demand_2 = _PMs.var(pm, nw_2, :z_demand, i)
+
+    JuMP.@constraint(pm.model, z_demand_2 >= z_demand_1)
 end
 
 
