0fe04f5abcca8ecb5c7a44a3d1bbe937dc03d22f
Noah Rhodes <noahx23@gmail.com>
2021-01-21 18:03:26 -0600

    Update iterative tests for removal of cardinal lb
    Objective of  DC example changed (fewere sub iterations)
    
    testing the number of repairs changed to >= instead of approx
    because of solutions allows delays of repairs that do not add
    to load served.

--- test/iterative.jl
+++ test/iterative.jl
@@ -8,18 +8,18 @@
 
             @test result["termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 1990.87; atol = 1e0)
-            @test isapprox(length(keys(result["solution"]["nw"])), 9; atol=1e0)
+            @test isapprox(length(keys(result["solution"]["nw"])), 9; atol=1e-1)
 
             #there should be a new active item in each time period
-            @test isapprox(count_active_items(result["solution"]["nw"]["0"]), 10, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["1"]), 14, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["2"]), 15, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["3"]), 16, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["4"]), 17, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["5"]), 18, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["6"]), 19, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["7"]), 20, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["8"]), 21, atol=1e0)
+            @test count_active_items(result["solution"]["nw"]["0"]) == 10
+            @test count_active_items(result["solution"]["nw"]["1"]) >= 15
+            @test count_active_items(result["solution"]["nw"]["2"]) >= 16
+            @test count_active_items(result["solution"]["nw"]["3"]) >= 17
+            @test count_active_items(result["solution"]["nw"]["4"]) >= 18
+            @test count_active_items(result["solution"]["nw"]["5"]) >= 19
+            @test count_active_items(result["solution"]["nw"]["6"]) >= 19
+            @test count_active_items(result["solution"]["nw"]["7"]) >= 19
+            @test count_active_items(result["solution"]["nw"]["8"]) == 22
 
             @test isapprox(gen_status(result,"0","1"), 0; atol=1e-2)
             @test isapprox(gen_status(result,"0","2"), 0; atol=1e-2)
@@ -62,18 +62,18 @@
 
             @test result["termination_status"] == LOCALLY_SOLVED
             @test isapprox(result["objective"], 1990.91; atol = 1e0)
-            @test isapprox(length(keys(result["solution"]["nw"])), 9; atol=1e0)
+            @test isapprox(length(keys(result["solution"]["nw"])), 9; atol=1e-1)
 
             #there should be a new active item in each time period
-            @test isapprox(count_active_items(result["solution"]["nw"]["0"]), 10, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["1"]), 14, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["2"]), 15, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["3"]), 16, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["4"]), 17, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["5"]), 18, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["6"]), 19, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["7"]), 20, atol=1e0)
-            @test isapprox(count_active_items(result["solution"]["nw"]["8"]), 21, atol=1e0)
+            @test count_active_items(result["solution"]["nw"]["0"]) == 10
+            @test count_active_items(result["solution"]["nw"]["1"]) >= 15
+            @test count_active_items(result["solution"]["nw"]["2"]) >= 16
+            @test count_active_items(result["solution"]["nw"]["3"]) >= 17
+            @test count_active_items(result["solution"]["nw"]["4"]) >= 17
+            @test count_active_items(result["solution"]["nw"]["5"]) >= 19
+            @test count_active_items(result["solution"]["nw"]["6"]) >= 19
+            @test count_active_items(result["solution"]["nw"]["7"]) >= 19
+            @test count_active_items(result["solution"]["nw"]["8"]) == 22
 
             @test isapprox(gen_status(result,"0","1"), 0; atol=1e-2)
             @test isapprox(gen_status(result,"0","2"), 0; atol=1e-2)
@@ -119,23 +119,23 @@
             result = PowerModelsRestoration.run_iterative_restoration(data, PowerModels.DCPPowerModel, cbc_solver, repair_periods=3)
 
             @test result["termination_status"] == OPTIMAL
-            @test isapprox(result["objective"], 755.72; atol = 1e0)
-            @test isapprox(length(keys(result["solution"]["nw"])), 13; atol=1e0)
+            @test isapprox(result["objective"], 550.04; atol = 1e0)
+            @test isapprox(length(keys(result["solution"]["nw"])), 13; atol=1e-1)
 
             #there should be a new active item in each time period
-            @test isapprox(count_active_items( result["solution"]["nw"]["0"]), 6, atol=1e0) #MLD is the first time period will disable isolated devices
-            @test isapprox(count_active_items( result["solution"]["nw"]["1"]), 10, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["2"]), 11, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["3"]), 12, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["4"]), 13, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["5"]), 14, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["6"]), 15, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["7"]), 16, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["8"]), 17, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["9"]), 18, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["10"]), 19, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["11"]), 20, atol=1e0)
-            @test isapprox(count_active_items( result["solution"]["nw"]["12"]), 21, atol=1e0)
+            @test count_active_items( result["solution"]["nw"]["0"]) == 6 #MLD is the first time period will disable isolated devices
+            @test count_active_items( result["solution"]["nw"]["1"]) >= 10
+            @test count_active_items( result["solution"]["nw"]["2"]) >= 10
+            @test count_active_items( result["solution"]["nw"]["3"]) >= 12
+            @test count_active_items( result["solution"]["nw"]["4"]) >= 14
+            @test count_active_items( result["solution"]["nw"]["5"]) >= 15
+            @test count_active_items( result["solution"]["nw"]["6"]) >= 16
+            @test count_active_items( result["solution"]["nw"]["7"]) >= 16
+            @test count_active_items( result["solution"]["nw"]["8"]) >= 16
+            @test count_active_items( result["solution"]["nw"]["9"]) >= 16
+            @test count_active_items( result["solution"]["nw"]["10"]) >= 16
+            @test count_active_items( result["solution"]["nw"]["11"]) >= 16
+            @test count_active_items( result["solution"]["nw"]["12"]) == 22
 
             # @test isapprox(gen_status(result,"0","1"), 0; atol=1e-2)
             # @test isapprox(gen_status(result,"0","2"), 0; atol=1e-2)
