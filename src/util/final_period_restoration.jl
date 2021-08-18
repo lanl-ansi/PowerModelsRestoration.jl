802e67601aad90cb0926d1243f7c46f577199603
Noah Rhodes <nrhodes@wisc.edu>
2021-08-18 15:10:20 -0500

    FIX: Final algo, change from damaged to repairable

--- src/util/final_period_restoration.jl
+++ src/util/final_period_restoration.jl
@@ -5,8 +5,8 @@ function final_period_restoration(network::Dict{String,<:Any})
     if _IM.ismultinetwork(network)
         Memento.error(_PM._LOGGER, "final_period_restoration requires a single network.")
     end
-    dmg_count = count_damaged_items(network)
-    dmg_items = get_damaged_items(network)
+    dmg_count = count_repairable_items(network)
+    dmg_items = get_repariable_items(network)
 
     restoration_order = Dict("$nw_id" => Dict(comp_type=>String[] for comp_type in restoration_comps) for nw_id in 1:dmg_count)
     restoration_order["$dmg_count"] = dmg_items
