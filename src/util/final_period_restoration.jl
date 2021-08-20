a4bbd91462d0c36fcf9d164754c3111a330908fb
Noah Rhodes <nrhodes@wisc.edu>
2021-08-20 11:20:45 -0500

    fix typo in Final

--- src/util/final_period_restoration.jl
+++ src/util/final_period_restoration.jl
@@ -6,7 +6,7 @@ function final_period_restoration(network::Dict{String,<:Any})
         Memento.error(_PM._LOGGER, "final_period_restoration requires a single network.")
     end
     dmg_count = count_repairable_items(network)
-    dmg_items = get_repariable_items(network)
+    dmg_items = get_repairable_items(network)
 
     restoration_order = Dict("$nw_id" => Dict(comp_type=>String[] for comp_type in restoration_comps) for nw_id in 1:dmg_count)
     restoration_order["$dmg_count"] = dmg_items
