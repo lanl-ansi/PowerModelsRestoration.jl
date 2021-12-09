a0b4d75118f6a617f8f3b9ea3ed0084f14d5b02e
Noah Rhodes <nrhodes@wisc.edu>
2021-12-09 17:56:40 -0600

    Add documentation to `apply_restoration_sequence`

--- src/core/data.jl
+++ src/core/data.jl
@@ -701,21 +701,28 @@ function add_load_weights!(data::Dict{String,<:Any})
     end
 end
 
-# """
-#     update device status and remove damaged indicator if item was repaired in network 3
-#     Before:
-#     | nw_id   |  1  |  2  |  3  |  4  |
-#     | ------- | --- | --- | --- | --- |
-#     | status  |  1  |  1  |  1  |  1  |
-#     | damaged |  1  |  1  |  1  |  1  |
 
-#     Repaired in network 3
-#     After:
-#     | nw_id   |  1  |  2  |  3  |  4  |
-#     | ------- | --- | --- | --- | --- |
-#     | status  |  0  |  0  |  1  |  1  |
-#     | damaged |  1  |  1  |  1  |  0  |
-# """
+"""
+    `apply_restoration_sequence!(data::Dict{String,<:Any}, repair_order::Dict{String,<:Any})`
+
+    Update devices status and damaged indicator based on the repair order.
+
+    E.g. if a device is repaired in network id 3, then before network 3 its status is `0`
+    and its `damaged` indicator is `1`.  In network 3, its status is `1` and its damaged status is `1`.
+    After network 3, its status is `1` and its damaged indicator is `0`.
+
+    Before:
+    | nw_id   |  1  |  2  |  3  |  4  |
+    | ------- | --- | --- | --- | --- |
+    | status  |  1  |  1  |  1  |  1  |
+    | damaged |  1  |  1  |  1  |  1  |
+
+    Applying repair in network 3:
+    | nw_id   |  1  |  2  |  3  |  4  |
+    | ------- | --- | --- | --- | --- |
+    | status  |  0  |  0  |  1  |  1  |
+    | damaged |  1  |  1  |  1  |  0  |
+"""
 function apply_restoration_sequence!(data::Dict{String,<:Any}, repair_order::Dict{String,<:Any})
     if !_IM.ismultinetwork(data)
         Memento.error(_PM._LOGGER, "Cannot apply restoration sequence.  Data is not a multinetwork")
@@ -724,7 +731,6 @@ function apply_restoration_sequence!(data::Dict{String,<:Any}, repair_order::Dic
     for (repair_nw_id, comp_data) in repair_order
         for (comp_type, comp_id) in comp_data
             status_key = _PM.pm_component_status[comp_type]
-            # for comp_id in comp_ids
                 for (nw_id, net) in data["nw"]
                     if  parse(Int,nw_id) < parse(Int,repair_nw_id)
                         net[comp_type][comp_id][status_key] = _PM.pm_component_status_inactive[comp_type]
@@ -732,7 +738,6 @@ function apply_restoration_sequence!(data::Dict{String,<:Any}, repair_order::Dic
                         net[comp_type][comp_id][status_key] = 1
                     end
                 end
-            # end
         end
     end
     return data
